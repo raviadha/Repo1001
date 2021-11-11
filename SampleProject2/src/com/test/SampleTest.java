82764f01bf0e27092272e550a7a2cf4f096a2678
raviadha <adha.raviadha@gmail.com>
2021-11-11 06:43:03 +0000

    this is my first commit

---
+++ SampleProject2/src/com/test/SampleTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author opc
+ *
+ */
+public class SampleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("first commit");
+
+	}
+
+}
