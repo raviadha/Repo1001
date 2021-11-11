f50998197c4ee0843a21cd4ad77316b8ed8587e0
raviadha <adha.raviadha@gmail.com>
2021-11-11 06:17:46 +0000

    newly added comment

--- SampleProject/src/com/test/SampleClass.java
+++ SampleProject/src/com/test/SampleClass.java
@@ -13,8 +13,7 @@ public class SampleClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Newly added comments for testing");
 	}
 
 }
