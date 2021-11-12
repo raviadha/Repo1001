f80eceb50fd11883ab94919b1ad9abf4910ac135
raviadha <adha.raviadha@gmail.com>
2021-11-12 05:18:21 +0000

    merging changes

--- SampleProject/src/com/test/SampleClass.java
+++ SampleProject/src/com/test/SampleClass.java
@@ -14,6 +14,8 @@ public class SampleClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is Dev branch commit..... additional comments");
+		
+		System.out.println("This is merging of dev-SIT changes.");
 	}
 
 }
